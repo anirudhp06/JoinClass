f5607c7b623ffae9ed790c096fc409a0f3263920
anirudhp06 <anirudhparvatikar12@gmail.com>
2022-03-03 08:23:45 +0530

    Updated Base.

--- JoinClass.py
+++ JoinClass.py
@@ -29,7 +29,7 @@ subject_name = [
     "Kannada",
     "CN",
 ]  # Setting up Subjects to be displayed
-
+subject_name=[each_string.lower() for each_string in subject_name]
 links = [
     "https://trimurl.co/3CGlxF",
     "https://trimurl.co/L0JHyM",
@@ -55,16 +55,15 @@ while True:  # Infite loop so program can run until user wants to...
 
         print(
             "{}.{}".format(sno, i)
-        )  # Printing Subjects that are present in variable 'a'
+        )  # Printing Subjects that are present in variable 'subject_name'
 
         sno += 1
 
     print("10.Exit")  # Displayed at last
-
-    select = int(input("Enter Your choice:"))
-
-    if select == 1:
-
+    ind=0
+    sub_sel = input("Enter Subject name(as is):")
+    if sub_sel in subject_name:
+        ind=subject_name.index(sub_sel)
         print("Do you want to schedule When to join?(Y/N)(y/n):")
         sec = input()
 
@@ -77,51 +76,13 @@ while True:  # Infite loop so program can run until user wants to...
 
             print(x.strftime("%I:%M %p"), end=" ")
 
-            print("Joining Java Class in {} mins".format(rem))
+            print("Joining {} Class in {} mins".format(sub_sel,rem))
 
             sleep(rem * 60)  # Sleep Function takes only seconds as inputs so multiplied 'rem' with 60
 
-            sys("cls")
+            sys(clear)
 
-            webbrowser.open(links[select - 1])  # Function which will accurately opens class in web browser
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Java Class")
-
-            webbrowser.open(links[select - 1])
-
-            sleep(1)
-
-            sys("cls")
-
-    elif select == 2:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Java Lab in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
+            webbrowser.open(links[ind])  # Function which will accurately opens class in web browser
 
         else:
 
@@ -129,283 +90,20 @@ while True:  # Infite loop so program can run until user wants to...
 
             print(x.strftime("%I:%M %p"), end=" ")
 
-            print("Joining Java-Lab")
+            print("Joining {} Class".format(sub_sel))
 
-            webbrowser.open(links[select - 1])
-
-            sleep(1)
-
-            sys("cls")
-
-    elif select == 3:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining OS in {} mins".format(rem))
-
-            sleep(rem * 60)
+            webbrowser.open(links[ind])
 
             sleep(1)
 
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining OS")
-
-            webbrowser.open(links[select - 1])
-
-            sleep(1)
-
-            sys("cls")
-
-    elif select == 4:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining UNIX  in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining UNIX")
-
-            webbrowser.open(links[select - 1])
-
-    elif select == 5:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining DBMS in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining DBMS")
-
-            webbrowser.open(links[select - 1])
-
-            sleep(1)
-
-            sys("cls")
-
-    elif select == 6:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining DBMS Lab in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining DBMS-Lab")
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-    elif select == 7:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining English in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining English Class")
-
-            webbrowser.open(links[select - 1])
-
-            sleep(1)
-
-            sys("cls")
-
-    elif select == 8:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Kannada Class in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Kannada Class")
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-    elif select == 9:
-
-        print("Do you want to schedule when to join?(Y/N)(y/n):")
-        sec = input()
-
-        if sec == "y" or sec == "Y":
-
-            print("After how much mins u want to join class? (in Mins):")
-            rem = int(input())
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Computer Networking Class in {} mins".format(rem))
-
-            sleep(rem * 60)
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-        else:
-
-            x = datetime.datetime.now()
-
-            print(x.strftime("%I:%M %p"), end=" ")
-
-            print("Joining Computer Networking Class")
-
-            sleep(1)
-
-            sys("cls")
-
-            webbrowser.open(links[select - 1])
-
-    elif select == 10:
+            sys(clear)
+    elif sub_sel == "exit" or "EXIT":
 
         print("Execution of program Stopped\n Closing Program in 3 Seconds")
 
         sleep(2)
 
-        system.exit()  # Exits program After 3 seconds of selecting option to exit.
+        system.exit()  # Exits program After 3 seconds of selecting option to exit."""
 
     else:
 
@@ -415,4 +113,4 @@ while True:  # Infite loop so program can run until user wants to...
 
         print("Try Again with Option")
 
-        sys("cls")  # Clears the user screen if user enters wrong option.
+        sys(clear)  # Clears the user screen if user enters wrong option.
